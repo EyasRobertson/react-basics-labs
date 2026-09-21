3381deb6c16e9fe2d793d9dd667989076e1e29ae
EyasRobertson <20112370@setu.ie>
2026-09-21 12:17:25 +0100

    Tasky Lab 2b: Exercise 2 completed

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -15,7 +15,8 @@ function App() {
 	const [ formState, setFormState ] = useState({
 		title: "",
 		description: "",
-		deadline: ""
+		deadline: "",
+		priority: ""
 	});
 	const doneHandler = (taskIndex) => {
 		const tasks = [...taskState.tasks];
@@ -40,6 +41,9 @@ function App() {
 			  break;
 		  case "deadline":
 			  form.deadline = event.target.value;
+			  break;
+		  case "priority":
+			  form.priority = event.target.value;
 			  break;
 		  default:
 			  form = formState;
